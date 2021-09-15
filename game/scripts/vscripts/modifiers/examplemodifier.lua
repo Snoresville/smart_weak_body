8127a1d9915186f5cc9d3e087c013abc2a4c3dda
Snoresville <reflectiveflawless@hotmail.com>
2021-09-15 21:28:04 +0930

    made stat bonuses update

--- game/scripts/vscripts/modifiers/examplemodifier.lua
+++ game/scripts/vscripts/modifiers/examplemodifier.lua
@@ -12,11 +12,20 @@ function examplemodifier:RemoveOnDeath() return false end
 function examplemodifier:IsHidden() return false end 	-- we can hide the modifier
 function examplemodifier:IsDebuff() return true end 	-- make it red or green
 
+function examplemodifier:OnCreated()
+	if IsClient() then return end
+	self:StartIntervalThink(0.1)
+end
+
 function examplemodifier:GetAttributes()
 	return 0
 		+ MODIFIER_ATTRIBUTE_PERMANENT           -- Modifier passively remains until strictly removed. 
 		-- + MODIFIER_ATTRIBUTE_MULTIPLE            -- Allows modifier to stack with itself. 
 		-- + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE -- Allows modifier to be assigned to invulnerable entities. 
+end
+
+function examplemodifier:OnIntervalThink()
+	self:GetParent():CalculateStatBonus(true)
 end
 
 function examplemodifier:DeclareFunctions()
